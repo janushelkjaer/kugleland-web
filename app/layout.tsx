8de0704b100981430d620dc3d574df922f3e5163
Janus Helkjær <janus.helkjaer@gmail.com>
2025-04-02 11:29:04 +0200

    landing page optimized

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./components/header";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,11 +27,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
+        <Header />
         {children}
 
-
+        <footer className="flex flex-col items-center justify-center w-full h-24 bg-gray-50 text-gray-500 mt-auto border-t border-gray-100 relative z-10">
+          <ul className="list-none flex items-center justify-center gap-2">
+            <li className="text-xs text-gray-500">© kugleland 2025</li>
+            <li className="text-gray-100"> | </li>
+            <li className="text-xs text-gray-500">CVR: DK 36373040</li>
+          </ul>
+        </footer>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,35 @@
+import { Particles } from "./components/ui/particles";
+
+
 export default function Home() {
+
   return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start pt-24">
-        <div className="relative isolate px-6 pt-14 lg:px-8">
-          <div
-            aria-hidden="true"
-            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          >
-            <div
-              style={{
-                clipPath:
-                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-              }}
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            />
-          </div>
-          <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+    <div className="flex flex-col items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
+      <main className="w-full flex-1">
+        <div className="relative isolate px-6 pt-14 lg:px-8 w-full z-10">
+
+          <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 relative z-10">
 
             <div className="text-center">
               <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
                 kugleland
               </h1>
-              <p className="mt-8 text-pretty text-lg font-normal text-gray-400 sm:text-xl/8">
+              <p className="mt-8 text-pretty text-lg font-normal text-gray-400 sm:text-md">
                 Det er okay at være fjollet...
               </p>
-              {/* <div className="mt-10 flex items-center justify-center gap-x-6">
-                <a
-                  href="#"
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Get started
-                </a>
-                <a href="#" className="text-sm/6 font-semibold text-gray-900">
-                  Learn more <span aria-hidden="true">→</span>
-                </a>
-              </div> */}
             </div>
           </div>
-          <div
-            aria-hidden="true"
-            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          >
-            <div
-              style={{
-                clipPath:
-                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-              }}
-              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            />
-          </div>
-        </div>
 
+        </div>
+        <Particles
+          className="absolute inset-0"
+          quantity={100}
+          ease={80}
+          color={"#000000"}
+          refresh
+        />
       </main>
-      <footer className="flex flex-col items-center justify-center w-full h-24 bg-gray-50 text-gray-500 mt-auto border-t border-gray-100">
-        <ul className="list-none flex items-center justify-center gap-2">
-          <li className="text-xs text-gray-500">© kugleland 2025</li>
-          <li className="text-gray-100"> | </li>
-          <li className="text-xs text-gray-500">CVR: DK 36373040</li>
-        </ul>
-      </footer>
+
     </div>
   );
 }
